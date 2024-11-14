cfd69661255a2afd473e0d464deda6a52b775e55
tyoon11 <mcvv0110@naver.com>
2024-11-14 16:31:03 +0900

    도커 구동 완료 커밋

--- myfastapi/database.py
+++ myfastapi/database.py
@@ -1,18 +1,28 @@
 import os
-from sqlalchemy import create_engine, MetaData
+import time
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
-# SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL") # DB 접속 주소
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:0110@localhost/myapp_sql"
-# DB 접속 객체(커넥션 풀) 생성
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True
-)
-# DB 접속 클래스 생성
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "mysql+pymysql://myapp-sql:123456@mysql/myapp_sql")
+
+# 데이터베이스 엔진 생성 함수에 재시도 로직 추가
+def create_db_engine():
+    while True:
+        try:
+            engine = create_engine(SQLALCHEMY_DATABASE_URL)
+            # 연결 확인을 위한 테스트 쿼리 실행
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))  # 수정된 부분
+            print("Successfully connected to the database.")
+            return engine
+        except OperationalError:
+            print("Database connection failed. Retrying in 5 seconds...")
+            time.sleep(5)
+
+engine = create_db_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# autocommit=False 데이터를 변경 했을 때 commit을 해야만 실제 저장
 Base = declarative_base()
 
 def get_db():
